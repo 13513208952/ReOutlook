70bc16d9a0492acc4bf2b2b43c3263e9276cb05e
zhao zo <13513208952@users.noreply.github.com>
2026-09-20 20:39:59 +0800

    Prepare v0.3.1-alpha release

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -10,8 +10,8 @@ android {
         applicationId = "io.github.reoutlook"
         minSdk = 26
         targetSdk = 36
-        versionCode = 3
-        versionName = "0.3.0-alpha"
+        versionCode = 4
+        versionName = "0.3.1-alpha"
     }
 
     buildTypes {
